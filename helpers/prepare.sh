539fd9f2f5ccd829cf8ade742a596e65ee5c8d63
Michael Spencer <sonrisesoftware@gmail.com>
2016-06-25 10:11:18 -0500

    Cleaned up prepare script

--- helpers/prepare.sh
+++ helpers/prepare.sh
@@ -27,70 +27,110 @@ os_dir=$1
 OSNAME=$2
 arch=$3
 
-# Set up the kernel/boot files
+# link /dir -> real_dir
+link() {
+    path = "os_dir"$1
+    rm -r $path || true
+    ln -s $2 $path
+}
 
-kernel_version=$(pacman -r "$os_dir" -Q linux | cut -d' ' -f 2)-ARCH
+# dir /dir
+dir() {
+    path = "os_dir"$1
+    mkdir -p $path
+}
 
-linux${arch} systemd-nspawn -D "$os_dir" \
-    mkinitcpio -c /etc/ostree-mkinitcpio.conf -g /boot/initramfs-linux.img \
-        -k $kernel_version -S autodetect
+# no_dir /dir
+no_dir() {
+    path = "os_dir"$1
+    rm -rf $path
+}
 
-boot_dir="$os_dir"/boot
+sync() {
+    path1 = "os_dir"$1
+    path2 = "os_dir"$2
+    rsync -av $path1 $path2
+}
 
-rm -f "$boot_dir"/initramfs-linux-fallback.img
+setup_boot() {
+    kernel_version=$(pacman -r "$os_dir" -Q linux | cut -d' ' -f 2)-ARCH
 
-checksum=$(cat "$boot_dir"/initramfs-* "$boot_dir"/vmlinuz-* | \
-           sha256sum | cut -d' ' -f 1)
+    linux${arch} systemd-nspawn -D "$os_dir" \
+        mkinitcpio -c /etc/ostree-mkinitcpio.conf -g /boot/initramfs-linux.img \
+            -k $kernel_version -S autodetect
 
-mv "$boot_dir"/initramfs-*.img "$boot_dir"/initramfs-$checksum
-mv "$boot_dir"/vmlinuz-* "$boot_dir"/vmlinuz-$checksum
+    boot_dir="$os_dir"/boot
 
-# Clean up the directory structure
+    rm -f "$boot_dir"/initramfs-linux-fallback.img
 
-# Doesn't seem to be needed any more...
-rm "$os_dir"/etc/pacman.d/gnupg/S.gpg-agent
+    checksum=$(cat "$boot_dir"/initramfs-* "$boot_dir"/vmlinuz-* | \
+               sha256sum | cut -d' ' -f 1)
 
-mkdir -p "$os_dir"/usr/etc/
-mkdir -p "$os_dir"/sysroot
-mkdir -p "$os_dir"/var/mnt
-mkdir -p "$os_dir"/var/srv
-mkdir -p "$os_dir"/var/opt
+    mv "$boot_dir"/initramfs-*.img "$boot_dir"/initramfs-$checksum
+    mv "$boot_dir"/vmlinuz-* "$boot_dir"/vmlinuz-$checksum
+}
 
-rsync -av "$os_dir"/etc "$os_dir"/usr/etc/
-rm -rf "$os_dir"/{home,opt,root,mnt,etc}
+setup_sysroot() {
+    dir /sysroot
+    link /ostree sysroot/ostree
+}
 
-ln -s sysroot/ostree "$os_dir"/ostree
-ln -s sysroot/tmp "$os_dir"/tmp
-ln -s var/home "$os_dir"/home
-ln -s var/opt "$os_dir"/opt
-ln -s var/srv "$os_dir"/srv
-ln -s var/mnt "$os_dir"/mnt
-ln -s var/local "$os_dir"/usr/local
-ln -s var/roothome "$os_dir"/root
+move_etc() {
+    dir /usr/etc
+    sync /etc /usr/etc
+    no_dir /etc
+}
 
-chmod a+rX "$os_dir"/{bin,boot,lib,sysroot,usr,var,opt,srv,mnt}
+setup_links() {
+    link /home var/home
+    link /opt var/opt
+    link /srv var/srv
+    link /root var/roothome
+    link /usr/local var/local
+    link /mnt var/mnt
+    link /tmp sysroot/tmp
+}
 
-# cat << EOF > "$os_dir"/usr/etc/fstab
-# LABEL=$OSNAME-boot /boot ext4 rw,relatime,data=ordered 0 2
-# LABEL=$OSNAME-root / ext4 rw,relatime,data=ordered 0 1
-# EOF
+setup_tmpfiles() {
+    cat << EOF > "$os_dir"/usr/lib/tmpfiles.d/ostree.conf
+    d /var/log/journal 0755 root root -
+    L /var/home - - - - ../sysroot/home
+    d /var/opt 0755 root root -
+    d /var/srv 0755 root root -
+    d /var/roothome 0700 root root -
+    d /var/local 0755 root root -
+    d /var/local/bin 0755 root root -
+    d /var/local/etc 0755 root root -
+    d /var/local/games 0755 root root -
+    d /var/local/include 0755 root root -
+    d /var/local/lib 0755 root root -
+    d /var/local/man 0755 root root -
+    d /var/local/sbin 0755 root root -
+    d /var/local/share 0755 root root -
+    d /var/local/src 0755 root root -
+    d /var/mnt 0755 root root -
+    d /run/media 0755 root root -
+    EOF
+}
 
-cat << EOF > "$os_dir"/usr/lib/tmpfiles.d/ostree.conf
-d /var/log/journal 0755 root root -
-L /var/home - - - - ../sysroot/home
-d /var/opt 0755 root root -
-d /var/srv 0755 root root -
-d /var/roothome 0700 root root -
-d /var/local 0755 root root -
-d /var/local/bin 0755 root root -
-d /var/local/etc 0755 root root -
-d /var/local/games 0755 root root -
-d /var/local/include 0755 root root -
-d /var/local/lib 0755 root root -
-d /var/local/man 0755 root root -
-d /var/local/sbin 0755 root root -
-d /var/local/share 0755 root root -
-d /var/local/src 0755 root root -
-d /var/mnt 0755 root root -
-d /run/media 0755 root root -
-EOF
+pre_cleanup() {
+    rm "$os_dir"/etc/pacman.d/gnupg/S.gpg-agent || true
+}
+
+post_cleanup() {
+    chmod a+rX "$os_dir"/{bin,boot,lib,sysroot,usr,var,opt,srv,mnt}
+}
+
+summary() {
+    ls "$os_dir"/{,root,var,usr,etc}
+}
+
+pre_cleanup
+setup_boot
+setup_sysroot
+move_etc
+setup_links
+setup_tmpfiles
+post_cleanup
+
+summary
